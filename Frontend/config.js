d014c0a926f622099d0eea8174fcde875a47420b
NAMERA NAYAT <103534492@student.swin.edu.au>
2025-10-10 06:57:50 +1100

    minor edits on the config file

--- Frontend/config.js
+++ Frontend/config.js
@@ -1,7 +1,7 @@
 // config.js
-// Dev over USB with Expo Go + adb reverse tcp:5001 tcp:5001
+// Dev over USB with Expo Go + adb reverse tcp:5000 tcp:5000
 
-export const BASE_URL = 'http://localhost:5001/';
+export const BASE_URL = 'http://localhost:5000/';
 
 // If you also have Socket.IO (or another realtime service) on 3001,
 // run: adb reverse tcp:3001 tcp:3001  and uncomment below.
