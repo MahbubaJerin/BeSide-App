a101574bc460f12e52a889e2ab836f0a38030d4e
NAMERA NAYAT <103534492@student.swin.edu.au>
2025-11-04 04:57:15 +1100

    update config.js

--- Frontend/config.js
+++ Frontend/config.js
@@ -10,12 +10,12 @@ if (__DEV__) {
   BASE_URL = "http://localhost:5000/";
 } else {
   // Production: Railway backend
-  BASE_URL = "https://beside-backend-2pdx.onrender.com";
+  BASE_URL = "https://beside-backend-2pdx.onrender.com/";
 }
 
 // If you also have Socket.IO (or another realtime service) on 3001,
 // run: adb reverse tcp:3001 tcp:3001  and uncomment below.
-// export const SOCKET_URL = __DEV__ ? 'http://localhost:3001/' : 'https://beside-production.up.railway.app/';
+// export const SOCKET_URL = __DEV__ ? 'http://localhost:3001/' : 'https://beside-backend-2pdx.onrender.com';
 
 // Optional default export (works if some files do import config from './config')
 export default {
